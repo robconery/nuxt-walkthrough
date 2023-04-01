34fa2c0af9feea0235fd432a22ecccc445bcf092
Rob Conery <rob@conery.io>
2023-04-01 12:06:20 -0700

    Episode one

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-layout>
+
+      <v-app-bar 
+        title="Application bar"
+        color="black"
+        flat
+        ></v-app-bar>
+
+      <v-main style="min-height: 300px;">
+        <NuxtPage />
+      </v-main>
+
+    </v-layout>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ['vuetify/lib/styles/main.sass'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
+})
